1810dae6ffe84a6e775eeefe659f31853eb9d206
Ram Reddy <rammohan@ensoftek.com>
2014-08-23 05:01:48 -0700

    MU2 modifications as per Brady's code review,

--- interface/patient_file/summary/add_edit_amendments.php
+++ interface/patient_file/summary/add_edit_amendments.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * Add/Edit Amendments
+ *
+ * Copyright (C) 2014 Ensoftek
+ *
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
+ *
+ * @package OpenEMR
+ * @author  Hema Bandaru <hemab@drcloudemr.com>
+ * @link    http://www.open-emr.org
+ */
 
 //SANITIZE ALL ESCAPES
 $sanitize_all_escapes=true;
@@ -16,7 +36,7 @@ $DateFormat=DateFormatRead();
 if ( isset($_POST['mode'] )) {
 	$currentUser = $_SESSION['authUserID'];
 	$created_time = date('Y-m-d H:i');
-	if ( formData("amendment_id") == "" ) {
+	if ( $_POST["amendment_id"] == "" ) {
 		// New. Insert
 		$query = "INSERT INTO amendments SET 
 			amendment_date = ?,
@@ -27,17 +47,18 @@ if ( isset($_POST['mode'] )) {
 			created_by = ?,
 			created_time = ?";
 		$sqlBindArray = array(
-			DateToYYYYMMDD( formData('amendment_date')),
-			formData('form_amendment_by'),
-			formData('form_amendment_status'),
+			DateToYYYYMMDD( $_POST['amendment_date']),
+			$_POST['form_amendment_by'],
+			$_POST['form_amendment_status'],
 			$pid,
-			formData('desc'),
+			$_POST['desc'],
 			$currentUser,
 			$create_time
 		);
+
 		$amendment_id = sqlInsert($query,$sqlBindArray);
 	} else {
-		$amendment_id = formData('amendment_id');
+		$amendment_id = $_POST['amendment_id'];
 		// Existing. Update
 		$query = "UPDATE amendments SET 
 			amendment_date = ?,
@@ -48,13 +69,13 @@ if ( isset($_POST['mode'] )) {
 			modified_time = ?
 			WHERE amendment_id = ?";
 		$sqlBindArray = array(
-			DateToYYYYMMDD(formData('amendment_date')),
-			formData('form_amendment_by'),
-			formData('form_amendment_status'),
-			formData('desc'),
+			DateToYYYYMMDD($_POST['amendment_date']),
+			$_POST['form_amendment_by'],
+			$_POST['form_amendment_status'],
+			$_POST['desc'],
 			$currentUser,
 			$create_time,
-			formData('amendment_id')
+			$_POST['amendment_id']
 		);
 		sqlStatement($query,$sqlBindArray);
 	}
@@ -68,12 +89,14 @@ if ( isset($_POST['mode'] )) {
 		created_time = ?";
 	$sqlBindArray = array(
 		$amendment_id,
-		formData('note','P',true),
-		formData("form_amendment_status"),
+		$_POST['note'],
+		$_POST["form_amendment_status"],
 		$currentUser,
 		$created_time
 	);
 	sqlStatement($query,$sqlBindArray);
+	header("Location:add_edit_amendments.php?id=$amendment_id");
+	exit;	
 }
 
 $amendment_id = ( $amendment_id ) ? $amendment_id : $_REQUEST['id'];
@@ -83,7 +106,7 @@ if ( $amendment_id ) {
 	$amendment_date = $resultSet['amendment_date'];
 	$amendment_status = $resultSet['amendment_status'];
 	$amendment_by = $resultSet['amendment_by'];
-	$amendment_desc = stripslashes($resultSet['amendment_desc']);
+	$amendment_desc = strip_escape_custom($resultSet['amendment_desc']);
 	
 	$query = "SELECT * FROM amendments_history ah INNER JOIN users u ON ah.created_by = u.id WHERE amendment_id = ? ";
 	$resultSet = sqlStatement($query,array($amendment_id));
@@ -114,6 +137,12 @@ $customAttributes = ( $onlyRead ) ? array("disabled" => "true") : null;
 }
 tr.selected {
   background-color: white;
+}
+.historytbl {
+ border-collapse: collapse;
+}
+.historytbl td th{
+  border: 1px solid #000; 
 }	
 </style>
 		
@@ -127,10 +156,10 @@ tr.selected {
 
 function formValidation() {
 	if ( $("#amendment_date").val() == "" ) {
-		alert("<?php xl('Select Amendment Date','e'); ?>");
+		alert("<?php echo xls('Select Amendment Date'); ?>");
 		return;
 	} else if ( $("#form_amendment_by").val() == "" ) {
-		alert("<?php xl('Select Requested By','e'); ?>");
+		alert("<?php echo xls('Select Requested By'); ?>");
 		return;
 	}
 
@@ -145,20 +174,20 @@ function formValidation() {
 
 <body class="body_top">
 
-<form action="add_edit_amendments.php" name="add_edit_amendments" id="add_edit_amendments" method="post">
+<form action="add_edit_amendments.php" name="add_edit_amendments" id="add_edit_amendments" method="post" onsubmit='return top.restoreSession()'>
 
 	<table>
 	<tr>
 		<td>
-			<span class="title"><?php echo htmlspecialchars( xl('Amendments'), ENT_NOQUOTES); ?></span>&nbsp;
+			<span class="title"><?php echo xlt('Amendments'); ?></span>&nbsp;
 		</td>
 		<?php if ( ! $onlyRead ) { ?>
 		<td>
-			<a href=# onclick="formValidation()" class="css_button_small"><span><?php echo xl('Save');?></span></a>
+			<a href=# onclick="formValidation()" class="css_button_small"><span><?php echo xlt('Save');?></span></a>
 		</td>
 		<?php } ?>
 		<td>
-			<a href="list_amendments.php" class="css_button_small"><span><?php echo xl('Back');?></span></a>
+			<a href="list_amendments.php" class="css_button_small"><span><?php echo xlt('Back');?></span></a>
 		</td>			
 	</tr>
 	</table>
@@ -166,14 +195,14 @@ function formValidation() {
 	<br>
     <table border=0 cellpadding=1 cellspacing=1>
 		<tr>
-			<td><span class=text ><?php echo htmlspecialchars(xl('Requested Date')); ?></span></td>
+			<td><span class=text ><?php echo xlt('Requested Date'); ?></span></td>
 			<td ><input type='text' size='10' name="amendment_date" id="amendment_date" readonly 
 						value='<?php echo $amendment_date ? htmlspecialchars( oeFormatShortDate($amendment_date), ENT_QUOTES) : oeFormatShortDate(); ?>'
     		/>
 			<?php if ( ! $onlyRead ) { ?>
          	<img src='<?php echo $rootdir; ?>/pic/show_calendar.gif' width='24' height='22'
     			id='img_amendment_date' valign="middle" border='0' alt='[?]' style='cursor:pointer;cursor:hand'
-    			title='<?php echo htmlspecialchars( xl('Click here to choose a date'), ENT_QUOTES); ?>'>
+    			title='<?php echo xlt('Click here to choose a date'); ?>'>
 			<script type="text/javascript">
 				Calendar.setup({inputField:"amendment_date", ifFormat:"<?php echo $DateFormat ?>", button:"img_amendment_date"});
 			</script>
@@ -182,43 +211,45 @@ function formValidation() {
 		</tr>
 		
 		<tr>
-			<td><span class=text ><?php echo htmlspecialchars(xl('Requested By')); ?></span></td>
+			<td><span class=text ><?php echo xlt('Requested By'); ?></span></td>
 			<td>
 				<?php echo generate_select_list("form_amendment_by", "amendment_from", $amendment_by,'Amendment Request By',' ','','','',$customAttributes); ?>
 			</td>
 		</tr>
 		
 		<tr>
-			<td><span class=text ><?php echo htmlspecialchars(xl('Request Description')); ?></span></td>
-			<td><textarea <?php echo ( $onlyRead ) ? "readonly" : "";  ?> id="desc" name="desc" rows="4" cols="30"><?php ($amendment_id) ? xl($amendment_desc,'e') : "" ; ?></textarea></td>
+			<td><span class=text ><?php echo xlt('Request Description'); ?></span></td>
+			<td><textarea <?php echo ( $onlyRead ) ? "readonly" : "";  ?> id="desc" name="desc" rows="4" cols="30"><?php 
+			if($amendment_id) { echo $amendment_desc; }else{ echo ""; } ?></textarea></td>
 		</tr>
 		
 		<tr>
-			<td><span class=text ><?php echo htmlspecialchars(xl('Request Status')); ?></span></td>
+			<td><span class=text ><?php echo xlt('Request Status'); ?></span></td>
 			<td>
 				<?php echo generate_select_list("form_amendment_status", "amendment_status", $amendment_status,'Amendment Status',' ','','','',$customAttributes); ?>
 			</td>
 		</tr>
 		
 		<tr>
-			<td><span class=text ><?php echo htmlspecialchars(xl('Comments')); ?></span></td>
-			<td><textarea <?php echo ( $onlyRead ) ? "readonly" : "";  ?> id="note" name="note" rows="4" cols="30"><?php ($amendment_id) ? "" : xl('New amendment request','e'); ?></textarea></td>
+			<td><span class=text ><?php echo xlt('Comments'); ?></span></td>
+			<td><textarea <?php echo ( $onlyRead ) ? "readonly" : "";  ?> id="note" name="note" rows="4" cols="30"><?php 
+			if($amendment_id) echo ""; else echo xlt('New amendment request'); ?></textarea></td>
 		</tr>
 	</table>
 	
 	<?php if ( $amendment_id ) { ?>
 	<hr>
 	
-	<span class="title"><?php echo xl("History") ; ?></span>
+	<span class="title"><?php echo xls("History") ; ?></span>
     
-	<table border="1" cellpadding=3 cellspacing=0>
+	<table border="1" cellpadding=3 cellspacing=0 class="historytbl">
 
     <!-- some columns are sortable -->
     <tr class='text bold'>
-		<th align="left" style="width:15%"><?php echo htmlspecialchars( xl('Date'), ENT_NOQUOTES); ?></th>
-		<th align="left" style="width:25%"><?php echo htmlspecialchars( xl('By'), ENT_NOQUOTES); ?></th>
-		<th align="left" style="width:15%"><?php echo htmlspecialchars( xl('Status'), ENT_NOQUOTES); ?></th>
-		<th align="left"><?php echo htmlspecialchars( xl('Comments'), ENT_NOQUOTES); ?></th>
+		<th align="left" style="width:15%"><?php echo xlt('Date'); ?></th>
+		<th align="left" style="width:25%"><?php echo xlt('By'); ?></th>
+		<th align="left" style="width:15%"><?php echo xlt('Status'); ?></th>
+		<th align="left"><?php echo xlt('Comments'); ?></th>
 	</tr>	
 
 	<?php 
@@ -226,11 +257,11 @@ function formValidation() {
 		while ( $row = sqlFetchArray($resultSet) ) {
 			$created_date = date('Y-m-d', strtotime($row['created_time']));
 			echo "<tr>";
-			$userName = $row['lname'] . ", " . $row['fname'];
-			echo "<td align=left class=text>" . htmlspecialchars(xl(oeFormatShortDate($created_date)),ENT_NOQUOTES) . "</td>";
-			echo "<td align=left class=text>" . htmlspecialchars(xl($userName),ENT_NOQUOTES) . "</td>";
+			$userName = text($row['lname']) . ", " . text($row['fname']);
+			echo "<td align=left class=text>" . oeFormatShortDate($created_date) . "</td>";
+			echo "<td align=left class=text>" . $userName . "</td>";
 			echo "<td align=left class=text>" . ( ( $row['amendment_status'] ) ? generate_display_field(array('data_type'=>'1','list_id'=>'amendment_status'), $row['amendment_status']) : '') . "</td>";
-			echo "<td align=left class=text>" . htmlspecialchars(xl($row['amendment_note']),ENT_NOQUOTES) . "</td>";
+			echo "<td align=left class=text>" . text($row['amendment_note']) . "</td>";
 			echo "<tr>";
 		}
 	 }
@@ -239,7 +270,7 @@ function formValidation() {
 	<?php } ?>
 	
 	<input type="hidden" id="mode" name="mode" value=""/>
-	<input type="hidden" id="amendment_id" name="amendment_id" value="<?php echo $amendment_id; ?>"/>
+	<input type="hidden" id="amendment_id" name="amendment_id" value="<?php echo attr($amendment_id); ?>"/>
 </form>
 </body>
 </html>
